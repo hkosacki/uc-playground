0e58f75a897da4a75923262ad02a8f16c5b2b6b3
Hubert Kosacki <hkosacki@gmail.com>
2020-08-03 16:43:36 +0200

    Dodany przycisk do uruchomienia swiatel

--- led-szlaban/main/main.ino
+++ led-szlaban/main/main.ino
@@ -3,19 +3,65 @@
 #define LED_1 4
 #define LED_2 7
 
+#define BUTTON 2
+
+bool shouldBlinkLed = false;
+
+const long BLINKING_TIME = 20000;
+const long BLINK_DELAY = 500;
+
+long startTime = 0;
+long blinkTime = 0;
+//ICACHE_RAM_ATTR
+void handleLedTrigger(){
+  Serial.println("Button pressed");
+  shouldBlinkLed = true;
+  startTime = millis();
+  blinkTime = startTime;
+}
+
+long measuredTime = 0;
+void blinkLed(){
+  measuredTime = millis();
+  if(measuredTime - blinkTime < BLINK_DELAY){
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, LOW);
+  }
+  else if(measuredTime - blinkTime < 2*BLINK_DELAY){
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, HIGH);
+  } else {
+    blinkTime = millis();
+  }
+  measuredTime = millis();
+  if(measuredTime - startTime >= BLINKING_TIME){
+    tearDownBlinking();
+  }
+}
+
+void tearDownBlinking(){
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  shouldBlinkLed = false;
+  startTime = 0;
+  blinkTime = 0;
+}
+
+
 void setup() {
+  Serial.begin(9600);
   pinMode(LED_1, OUTPUT);
   pinMode(LED_2, OUTPUT);
+  pinMode(BUTTON, INPUT);
 
   digitalWrite(LED_1, LOW);
   digitalWrite(LED_2, LOW);
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON), handleLedTrigger, RISING);
 }
 
 void loop() {
-  digitalWrite(LED_1, HIGH);
-  digitalWrite(LED_2, LOW);
-  delay(500);
-  digitalWrite(LED_1, LOW);
-  digitalWrite(LED_2, HIGH);
-  delay(500);
+  if(shouldBlinkLed){ 
+    blinkLed();
+  }
 }
